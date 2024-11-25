e088eebbb3842cc4433213437d287270fd9f6bfe
Inzamam <inzamamchowdhury20@gmail.com>
2024-11-26 01:20:26 +0600

    Fix : env

--- Services/aimodel.ts
+++ Services/aimodel.ts
@@ -11,7 +11,7 @@ console.log(apiKey)
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash-8b",
+  model: "gemini-1.5-flash",
 });
 
 const generationConfig = {
