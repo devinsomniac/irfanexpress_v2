50c3a508b87730178633b6f3cbe438d46d3923fb
Inzamam <inzamamchowdhury20@gmail.com>
2024-11-26 12:33:34 +0600

    Added Authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Image from 'next/image'
-import { Button } from './ui/button'
+import React from 'react';
+import Image from 'next/image';
+import { Button } from './ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,13 +8,14 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { auth, signIn, signOut } from '@/auth';
 
-
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
   return (
-    <div className=' p-2 flex justify-between items-center'>
+    <div className='p-2 flex justify-between items-center'>
       <Image src={"/logo.png"} alt='logo' height={100} width={100} />
       <div className='justify-between items-center w-full hidden md:flex'>
         <ul className='flex gap-4 ml-[400px]'>
@@ -23,26 +24,37 @@ const Navbar = () => {
           <li>About</li>
           <li>Contact Us</li>
         </ul>
-        <Button>Log In</Button>
+        {session ? (
+          <form action={async () => {
+            "use server";
+            await signOut({ redirectTo: "/" });
+          }}>
+            <Button type='submit'>Log Out</Button>
+          </form>
+        ) : (
+          <form action={async () => {
+            "use server";
+            await signIn("google");
+          }}>
+            <Button type='submit'>Log In</Button>
+          </form>
+        )}
       </div>
       <div className='md:hidden mr-5'>
-      <DropdownMenu>
-        <DropdownMenuTrigger><GiHamburgerMenu /></DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>Home</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Appointment</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>About</DropdownMenuItem>
-          <DropdownMenuItem>Log In</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-
+        <DropdownMenu>
+          <DropdownMenuTrigger><GiHamburgerMenu /></DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Home</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Appointment</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>About</DropdownMenuItem>
+            <DropdownMenuItem>Log In</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-      
     </div>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
