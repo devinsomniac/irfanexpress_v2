6c2c32ed491100585461d3b1c13ba292123899f0
Inzamam <inzamamchowdhury20@gmail.com>
2025-03-13 00:12:39 +0600

    feat : vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <Navbar/>
         {children}
+        <Analytics/>
       </body>
       </SessionProvider>
     </html>
