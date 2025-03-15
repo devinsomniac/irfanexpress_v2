60211791793216e9b795a3159a16874721526131
Inzamam <inzamamchowdhury20@gmail.com>
2025-03-15 11:58:16 +0600

    feat : Shuvo mobile number

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -81,7 +81,7 @@ const ContactPage: React.FC = () => {
     <>
       <div className="min-h-screen bg-gray-100 grid grid-cols-1 md:grid-cols-2 p-8">
         <div className="flex p-4 justify-center items-center h-full w-full">
-          <Card className="h-[700px] w-full">
+          <Card className="h-[750px] w-full">
             <CardHeader>
               <CardTitle className="text-2xl font-bold text-center">Contact Us</CardTitle>
             </CardHeader>
@@ -192,6 +192,23 @@ const ContactPage: React.FC = () => {
                         strokeLinecap="round"
                         strokeLinejoin="round"
                         strokeWidth="2"
+                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                      />
+                    </svg>
+                    +8801608268329
+                  </p>
+                  <p className="flex items-center">
+                    <svg
+                      className="h-5 w-5 mr-2 text-gray-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                       />
                     </svg>
@@ -231,7 +248,7 @@ const ContactPage: React.FC = () => {
             alt="Random Image"
             height={300}
             width={1000}
-            className="h-[700px] object-cover overflow-hidden rounded-2xl"
+            className="h-[750px] object-cover overflow-hidden rounded-2xl"
           />
         </div>
       </div>
